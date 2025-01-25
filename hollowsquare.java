2724e5357935114f0181f36dd3a7c2294f0f50c1
prabhat-858858 <prabhatkumar145231@gmail.com>
2025-01-25 12:11:02 +0530

    Add files via upload

---
+++ hollowsquare.java
@@ -0,0 +1,23 @@
+public class hollowsquare {
+    public static void main(String[]args){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            if((i==1)||(i==n)){
+                for(int j=1;j<=n;j++){
+                    System.out.print(j+" ");
+                }
+            }
+        else{
+            for(int k=1;k<=n;k++){
+                if(k==1||k==n){
+                    System.out.print(k+" ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+        }
+        System.out.println("");
+        }
+    }
+}
